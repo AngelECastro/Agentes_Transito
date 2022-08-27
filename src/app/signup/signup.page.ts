f3edfe4a9ac18c8955b6499e9c59399d0a128eba
David1897 <93883177+David1897@users.noreply.github.com>
2022-08-27 09:26:25 -0400

    La pagina del registro

---
+++ src/app/signup/signup.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../shared/user.class';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+})
+export class SignupPage implements OnInit {
+  user: User = new User();
+
+  constructor(private authSvc: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  async onRegister(){
+    const user = await this.authSvc.onRegistrar(this.user);
+    if (user){
+      console.log('Registrado correctamente');
+      this.router.navigateByUrl('/');
+    }
+  }
+
+}
